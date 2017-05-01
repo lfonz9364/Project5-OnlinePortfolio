c0c886f60f64774e4bd7305cfc99a443f072d9c4
Alfons Caroles <cllin7787@gmail.com>
2017-05-02 09:44:40 +1000

    Publish project

--- test/passwordMeter.test.js
+++
@@ -1,8 +0,0 @@
-var expect = require('chai').expect
-import passwordMeter from '../app/lib/passwordMeter'
-
-describe('passwordMeter', () => {
-  it('should return weak by default', () => {
-    expect( passwordMeter('') ).to.be.equal('worst')
-  })
-})
